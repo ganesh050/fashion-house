1dc58aa8abd056af15270cd60ed1f4173b1afeb7
Sabin Sunar <sbnsunar@gmail.com>
2021-03-05 22:44:09 -0600

    Merge branch 'master' of https://github.com/sbnsun/fashion-house

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,31 +1,18 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <footer>
-      <section class="footer--column footer--col-tandc">
-	    <ul class="footer--mini-menu legal-links">
-	        <li>
-	            <a href="/g/terms-conditions">
-	                Terms &amp; Conditions
-	            </a>
-          </li>
-          <li>
-            <a href="/g/create-ads">
-              Create Ads
-            </a>
-          </li>
-          <li>
-            <a href="/g/privacy-policy">
-              Privacy Policy
-            </a>
-          </li>
-	        <li>
+    <footer name = "fixed-footer">
+      <div class="py-2 text-center">
+        <br/>
+        <a href = "./">Terms &amp; Conditions</a>  
+        <a href = "./">Create Ads </a>
+        <a href="./"> Privacy Policy </a>
+      </div>
+      <div class="py-2 text-center font-tahoma font-weight-bold">
             Copyright &copy; 2021 FashionHouse. All rights reserved.
-	        </li>
-	    </ul>
-    </section>
+      </div>
     </footer>
   );
 };
