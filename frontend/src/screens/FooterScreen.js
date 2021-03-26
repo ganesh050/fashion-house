20ab8aa822c84134b0d0ae20c7819fd5f4b74fcd
Sabin Sunar <sbnsunar@gmail.com>
2021-03-26 10:31:50 -0500

    changes

--- frontend/src/screens/FooterScreen.js
+++ frontend/src/screens/FooterScreen.js
@@ -1,13 +1,9 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import  Footer from '../components/Footer'
+import React from "react";
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const FooterScreen = () => {
-    return (
-        <div>
-            Privacy Policy
-        </div>
-    )
-}
+  return <div>Privacy Policy</div>;
+};
 
-export default FooterScreen
+export default FooterScreen;
